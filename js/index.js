a1ca52583dd8e0e068844ee8741c88ce300570e9
MaybeNotABob <wesley2511@gmail.com>
2020-08-31 23:04:28 +0100

    started work on CSS file: adjusting to different resolutions using viewport/grid added headers to each file

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/*
+  Description:
+    Javascript file: 
+    Dynamically interacting with HTML elements to load images.
+
+	Version:
+		0.1a
+
+  Creation date:
+		31AUG2020
+
+  Contributors
+		MaybeNotABob
+
+*/
